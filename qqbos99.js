d5636d504e5c6cd2b5e8e070ae21f78320786117
qqbos99 <qqbos999@gmail.com>
2024-11-21 20:27:40 +0800

    Add files via upload

---
+++ qqbos99.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(event) { document.createElement('form').submit.call(document.getElementById('yourform')); }); 
+
+function change() {location.replace("#");}
+function opennew() {window.open("#");}
+function qqbos99jump() {window.open("#");}
