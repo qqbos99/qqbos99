afe48460fe9d6936ed92c620decf5f2e1f2933b1
qqbos99 <qqbos999@gmail.com>
2024-11-21 20:53:07 +0800

    Update qqbos99.js

--- qqbos99.js
+++ qqbos99.js
@@ -2,4 +2,5 @@ document.addEventListener("DOMContentLoaded", function(event) { document.createE
 
 function change() {location.replace("#");}
 function opennew() {window.open("#");}
-function qqbos99jump() {window.open("#");}
+function qqbos99jump() {window.open("https://qqbos99dana.com/");}
+function qqbos99change() {location.replace("https://qqbos99dana.com/");}
