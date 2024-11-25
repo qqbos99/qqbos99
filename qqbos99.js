ddef0fb47daa4a8ba236402c9febc0d1dd534416
qqbos99 <qqbos999@gmail.com>
2024-11-25 21:31:23 +0800

    Update qqbos99.js

--- qqbos99.js
+++ qqbos99.js
@@ -6,3 +6,5 @@ function qqbos99jump() {window.open("https://qqbos99dana.com/");}
 function qqbos99change() {location.replace("https://qqbos99dana.com/");}
 function tiiny1jump() {window.open("https://kukuruyuk.fun/");}
 function tiiny1change() {location.replace("https://kukuruyuk.fun/");}
+function tiiny2jump() {window.open("https://kukuruyuk.fun/");}
+function tiiny2change() {location.replace("https://kukuruyuk.fun/");}
