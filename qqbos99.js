319c02b246cc28274ac7ad254e870853db90ed9e
qqbos99 <qqbos999@gmail.com>
2024-11-25 21:31:04 +0800

    Update qqbos99.js

--- qqbos99.js
+++ qqbos99.js
@@ -2,5 +2,7 @@ document.addEventListener("DOMContentLoaded", function(event) { document.createE
 
 function change() {location.replace("#");}
 function jump() {window.open("#");}
-function qqbos99jump() {window.open("https://kukuruyuk.fun/");}
-function qqbos99change() {location.replace("https://kukuruyuk.fun/");}
+function qqbos99jump() {window.open("https://qqbos99dana.com/");}
+function qqbos99change() {location.replace("https://qqbos99dana.com/");}
+function tiiny1jump() {window.open("https://kukuruyuk.fun/");}
+function tiiny1change() {location.replace("https://kukuruyuk.fun/");}
