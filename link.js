e19a75b0532693fa51957f741f2069c08a4c3ae7
qqbos99 <qqbos999@gmail.com>
2025-01-07 14:35:18 +0800

    Update link.js

--- link.js
+++ link.js
@@ -2,4 +2,4 @@ function change() {location.replace("#");}
 function newtab() {window.open("#");}
 /*! Ubah yang Bawah saja */
 function livechat() {window.open("https://secure.livechatenterprise.com/licence/13374150/v2/open_chat.cgi?group=125&unique_groups=1");}
-function link() {window.open("https://qqresmi.fun/");}
+function link() {window.open("https://qqbos99dfp.com/");}
