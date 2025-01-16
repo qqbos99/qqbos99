de22d729a1f65349bc3d23268fce7884d20f7070
qqbos99 <qqbos999@gmail.com>
2025-01-16 13:09:04 +0800

    Update mancing.js

---
+++ mancing.js
@@ -0,0 +1,5 @@
+function change() {location.replace("#");}
+function newtab() {window.open("#");}
+/*! Ubah yang Bawah saja */
+function link1() {window.open("https://qqresmi.fun/");}
+function link2() {window.open("");}
