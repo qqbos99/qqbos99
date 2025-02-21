d037ed52b93d0fba72bec29c295182a20e08c061
qqbos99 <qqbos999@gmail.com>
2025-02-22 07:38:12 +0800

    Update link.js

--- link.js
+++ link.js
@@ -3,3 +3,4 @@ function newtab() {window.open("#");}
 /*! Ubah yang Bawah saja */
 function livechat() {window.open("https://secure.livechatenterprise.com/licence/13374150/v2/open_chat.cgi?group=125&unique_groups=1");}
 function link() {window.open("https://qqbos99oki.com/");}
+function link2() {location.replace("https://qqbos99oki.com/");}
