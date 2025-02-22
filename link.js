a06ac14a04fc3e38ce173809d1aac75127bcbcde
qqbos99 <qqbos999@gmail.com>
2025-02-22 08:24:40 +0800

    Update link.js

--- link.js
+++ link.js
@@ -2,5 +2,5 @@ function change() {location.replace("#");}
 function newtab() {window.open("#");}
 /*! Ubah yang Bawah saja */
 function livechat() {window.open("https://secure.livechatenterprise.com/licence/13374150/v2/open_chat.cgi?group=125&unique_groups=1");}
-function link() {window.open("https://qqbos99oki.com/");}
-function link2() {location.replace("https://qqbos99oki.com/");}
+function link() {window.open("https://qqresmi.fun/");}
+function link2() {location.replace("https://qqresmi.fun/");}
