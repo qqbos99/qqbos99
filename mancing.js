6d6327f1d2822f96b9037690d5bde0b2890b6bb2
qqbos99 <qqbos999@gmail.com>
2025-04-24 07:59:49 +0800

    Update mancing.js

--- mancing.js
+++ mancing.js
@@ -1,5 +1,5 @@
 function change() {location.replace("#");}
 function newtab() {window.open("#");}
 /*! Ubah yang Bawah saja */
-function link1() {window.open("https://qqbos99kpi.com/");}
-function link2() {window.open("https://qqbos99kpi.com/");}
+function link1() {window.open("https://senisastra.com/");}
+function link2() {window.open("https://senisastra.com/");}
