b535492bb3b8a7abda2e93f76a7d151cf882c5d2
qqbos99 <qqbos999@gmail.com>
2025-05-04 09:46:54 +0800

    Update link.js

--- link.js
+++ link.js
@@ -4,5 +4,7 @@ function newtab() {window.open("#");}
 function livechat() {window.open("https://secure.livechatenterprise.com/licence/13374150/v2/open_chat.cgi?group=125&unique_groups=1");}
 function link() {window.open("https://senisastra.com/");}
 function link2() {location.replace("https://senisastra.com/");}
-function linkfw() {window.open("https://qqfunworld.com/");}
-
+function linkfqqfunworld() {window.open("https://qqfunworld.com/");}
+function linkfhappy21() {window.open("https://qqhappy21dd.com/");}
+function linkfliga() {window.open("https://qqligaikn.com/");}
+function linkfw() {window.open("https://qqlikebetgtm.com/");}
