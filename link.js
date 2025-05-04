062640ea3fd9b63a9b25ca6e716f4a3d43d03e51
qqbos99 <qqbos999@gmail.com>
2025-05-04 09:44:00 +0800

    Update link.js

--- link.js
+++ link.js
@@ -4,3 +4,5 @@ function newtab() {window.open("#");}
 function livechat() {window.open("https://secure.livechatenterprise.com/licence/13374150/v2/open_chat.cgi?group=125&unique_groups=1");}
 function link() {window.open("https://senisastra.com/");}
 function link2() {location.replace("https://senisastra.com/");}
+function linkfw() {window.open("https://qqfunworld.com/");}
+
