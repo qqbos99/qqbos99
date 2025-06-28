51c2f8fed2f7e9c93e3aad5558feed6b548ac856
qqbos99 <qqbos999@gmail.com>
2025-06-28 10:52:55 +0800

    Update link.js

--- link.js
+++ link.js
@@ -2,6 +2,6 @@ function change() {location.replace("#");}
 function newtab() {window.open("#");}
 /*! Ubah yang Bawah saja */
 function livechat() {window.open("https://secure.livechatenterprise.com/licence/13374150/v2/open_chat.cgi?group=125&unique_groups=1");}
-function link() {window.open("https://rebrand.ly/qb1");}
-function link2() {location.replace("https://rebrand.ly/qb1");}
+function link() {window.open("https://qqbos99srp.com/");}
+function link2() {location.replace("https://qqbos99srp.com/");}
 
