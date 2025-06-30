96b5d8e4fefcae7597235106f28a46d814a23648
qqbos99 <qqbos999@gmail.com>
2025-06-30 08:17:20 +0800

    Delete mancing.js

--- mancing.js
+++
@@ -1,5 +0,0 @@
-function change() {location.replace("#");}
-function newtab() {window.open("#");}
-/*! Ubah yang Bawah saja */
-function link1() {window.open("https://senisastra.com/");}
-function link2() {window.open("https://senisastra.com/");}
