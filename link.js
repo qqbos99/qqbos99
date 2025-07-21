b7fad27d7d60463de253808d1ed4a990c510fe5f
qqbos99 <qqbos999@gmail.com>
2025-07-22 07:58:33 +0800

    Update link.js

--- link.js
+++ link.js
@@ -2,7 +2,7 @@ function change() {location.replace("#");}
 function newtab() {window.open("#");}
 /*! Ubah yang Bawah saja */
 function livechat() {window.open("https://secure.livechatenterprise.com/licence/13374150/v2/open_chat.cgi?group=125&unique_groups=1");}
-function link() {window.open("https://qqbos99srp.com/");}
-function link2() {location.replace("https://qqbos99srp.com/");}
+function link() {window.open("https://qqbos99p1.com/");}
+function link2() {location.replace("https://qqbos99p1.com/");}
 function linklikebet() {window.open("https://qqlikebetgtm.com/");}
 
