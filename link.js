f27cc187caae2643b7bbed87cb76f3d1105e2be0
qqbos99 <qqbos999@gmail.com>
2025-07-28 22:54:05 +0800

    Update link.js

--- link.js
+++ link.js
@@ -4,5 +4,5 @@ function newtab() {window.open("#");}
 function livechat() {window.open("https://secure.livechatenterprise.com/licence/13374150/v2/open_chat.cgi?group=125&unique_groups=1");}
 function link() {window.open("https://qqbos99p1.com/");}
 function link2() {location.replace("https://qqbos99p1.com/");}
-function linklikebet() {window.open("https://qqlikebetgtm.com/");}
+
 
