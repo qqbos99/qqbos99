06fa48d000c6676cdde52b67b8e7c6708221ffe0
qqbos99 <qqbos999@gmail.com>
2025-10-18 02:02:27 +0800

    Update link.js

--- link.js
+++ link.js
@@ -3,7 +3,8 @@ function change() {location.replace("#");}
 function newtab() {window.open("#");}
 /*! Ubah yang Bawah saja */
 function livechat() {window.open("https://secure.livechatenterprise.com/licence/13374150/v2/open_chat.cgi?group=125&unique_groups=1");}
-function link() {window.open("https://qqbos99p1.com/");}
-function link2() {location.replace("https://qqbos99p1.com/");}
-function linkps() {location.replace("https://qqbos99p1.com/");}
+function link() {window.open("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
+function link2() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
+function linkps() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
+
 
