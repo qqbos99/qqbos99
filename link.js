3d1584692a43c5d02b1184e545e0d811473aa0dc
qqbos99 <qqbos999@gmail.com>
2025-10-18 02:04:06 +0800

    Update link.js

--- link.js
+++ link.js
@@ -3,8 +3,9 @@ function change() {location.replace("#");}
 function newtab() {window.open("#");}
 /*! Ubah yang Bawah saja */
 function livechat() {window.open("https://secure.livechatenterprise.com/licence/13374150/v2/open_chat.cgi?group=125&unique_groups=1");}
-function link() {window.open("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
-function link2() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
-function linkps() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
+function link() {window.open("https://www.senisastra.com/");}
+function link2() {location.replace("https://www.senisastra.com/");}
+function linkps() {location.replace("https://www.senisastra.com/");}
+
 
 
