c64a7e41415c9a2470dfda616ad5803c23334259
qqbos99 <qqbos999@gmail.com>
2025-12-07 06:35:57 +0800

    Update link.js

--- link.js
+++ link.js
@@ -1,11 +1,10 @@
 document.getElementById('yourform').submit();
-function change() {location.replace("#");}
-function newtab() {window.open("#");}
-/*! Ubah yang Bawah saja */
 function livechat() {window.open("https://secure.livechatenterprise.com/licence/13374150/v2/open_chat.cgi?group=125&unique_groups=1");}
 function link() {location.replace("https://www.senisastra.com/");}
 function link2() {location.replace("https://www.senisastra.com/");}
+function link3() {window.open("https://www.senisastra.com");}
 function linkps() {location.replace("https://www.senisastra.com/");}
+
 
 
 
