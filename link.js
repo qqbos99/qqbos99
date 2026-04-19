a2cad7ae6f89c60539c90587ba5f25b1324f1311
qqbos99 <qqbos999@gmail.com>
2026-04-20 07:11:22 +0800

    Update link.js

--- link.js
+++ link.js
@@ -1,9 +1,9 @@
 document.getElementById('yourform').submit();
 function livechat() {window.open("https://secure.livechatenterprise.com/licence/13374150/v2/open_chat.cgi?group=125&unique_groups=1");}
 function link() {location.replace("https://www.senisastra.com/");}
-function link2() {location.replace("https://www.senisastra.com/");}
+function link2() {location.replace("https://amplob-ri.pages.dev/link/");}
 function link3() {window.open("https://www.senisastra.com");}
-function linkps() {location.replace("https://www.senisastra.com/");}
+function linkps() {location.replace("https://amplob-ri.pages.dev/link/");}
 
 
 
